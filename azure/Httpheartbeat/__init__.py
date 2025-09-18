fe4379d921b92c788d5f5ab3f64f1409f4907269
Wai Yin Fok <wy.fok1991@gmail.com>
2025-09-18 14:49:47 +0200

    initial commit

---
+++ azure/Httpheartbeat/__init__.py
@@ -0,0 +1,23 @@
+import azure.functions as func 
+import os 
+
+SECRET = os.environ("HTTPHEARTBEAT_SECRET")
+
+def main(req: func.HttpRequest) -> func.HttpRequest: 
+    """This is to receive Http Request from AWS Lambda.
+    Auth token is pulled from the Request to verify that the Http Request is from AWS Lambda. 
+    If the token does not match with the secret stored in env, respond "Forbidden" with 403 status code. 
+    If the token matches with the secrte, respond "Alive" with 200 status code. 
+
+    Args:
+        req (func.HttpRequest): Http Request 
+
+    Returns:
+        func.HttpRequest: Response string with status code 
+    """
+    request_token = req.headers.get("x-auth-token")
+    if request_token!=SECRET:
+        return func.HttpResponse("Azure response: Forbidden", status_code=403)    
+    
+    return func.HttpResponse("Azure response: Alive", status_code=200)
+
