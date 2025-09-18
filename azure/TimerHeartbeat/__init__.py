5083f1d210619e53e06d83de0b93e0255daf8138
Wai Yin Fok <wy.fok1991@gmail.com>
2025-09-18 15:45:16 +0200

    commenting

--- azure/TimerHeartbeat/__init__.py
+++ azure/TimerHeartbeat/__init__.py
@@ -7,26 +7,34 @@ from requests.exceptions import Timeout
 AWS_ENDPOINT = os.environ.get("AWS_ENDPOINT")
 SECRET = os.environ.get("HTTPHEARTBEAT_SECRET")
 
-def main(mytimer):
-    utc_time = datetime.datetime.now(datetime.timezone.utc).isoformat()
 
+def main(mytimer):
+    """Send out a Http Request to AWS and listen to response
+    Response is logged.
+
+    Args:
+        mytimer (_type_): Timer Trigger for Azure function.
+    """
+    utc_time = datetime.datetime.now(datetime.timezone.utc).isoformat()
+    # Send Http Get request to AWS Lambda with secret
+    # If the status code of the response is 200, consider the request successful. Else failed.
     try:
-        r = requests.get(
-            AWS_ENDPOINT,
-            headers={"x-auth-token": SECRET},
-            timeout=10
-        )
+        r = requests.get(AWS_ENDPOINT, headers={"x-auth-token": SECRET}, timeout=10)
         latency = r.elapsed.total_seconds() * 1000
         if r.status_code == 200:
-            logging.info(f"[Azure→AWS] {utc_time} Success {r.status_code}, {latency:.2f} ms")
+            logging.info(
+                f"[Azure→AWS] {utc_time} Success {r.status_code}, {latency:.2f} ms"
+            )
         else:
-            logging.warning(f"[Azure→AWS] {utc_time} Failed {r.status_code}, {latency:.2f} ms")
-
+            logging.warning(
+                f"[Azure→AWS] {utc_time} Failed {r.status_code}, {latency:.2f} ms"
+            )
+    # Capture Timeout error
     except Timeout as e:
         logging.error(f"[Azure→AWS] {utc_time} Timeout {e}")
-
+    # Capture all other unexpected errors.
     except Exception as e:
         logging.error(f"[Azure→AWS] {utc_time} Error {e}")
-
+    # Log for completion
     finally:
         logging.info(f"[Azure→AWS] {utc_time} Completed.")
