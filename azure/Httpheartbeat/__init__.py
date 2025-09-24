66fcdbde953c3828aedc188b390e62fe87349840
Wai Yin Fok <wy.fok1991@gmail.com>
2025-09-24 16:48:20 +0200

    Change typo

--- azure/Httpheartbeat/__init__.py
+++ azure/Httpheartbeat/__init__.py
@@ -8,7 +8,7 @@ def main(req: func.HttpRequest) -> func.HttpRequest:
     """This is to receive Http Request from AWS Lambda.
     Auth token is pulled from the Request to verify that the Http Request is from AWS Lambda.
     If the token does not match with the secret stored in env, respond "Forbidden" with 403 status code.
-    If the token matches with the secrte, respond "Alive" with 200 status code.
+    If the token matches with the secret, respond "Alive" with 200 status code.
 
     Args:
         req (func.HttpRequest): Http Request
